69c32efb8b9d9a79b3fc86817fd1b64ac5645d72
"Shane Holloway ext:(%22) <shane.holloway@ieee.org>
2010-01-13 16:36:26 -0700

    Refactored Workspace to split into base and sqlite backends

---
+++ be/base/workspace.py
@@ -0,0 +1,130 @@
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Imports 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#~ Definitions 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+class WorkspaceBase(object):
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~ Checkout and Working Changesets
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def _asChangeset(self, cs=False):
+        if cs is False:
+            return self.cs
+        raise NotImplementedError('Subclass Responsibility: %r' % (self,))
+
+    csCheckout = None
+
+    _csWorking = None
+    def getCSWorking(self, create=True):
+        cs = self._csWorking
+        if cs is None and create:
+            cs = self._createWorkingChangeset()
+            self._csWorking = cs
+        return cs
+    def setCSWorking(self, cs):
+        self._csWorking = cs
+    csWorking = property(getCSWorking, setCSWorking)
+
+    def getCS(self):
+        r = self._csWorking 
+        if r is None:
+            r = self.csCheckout
+        return r
+    def setCS(self, cs):
+        self.checkCommited()
+        if self.csCheckout is not None:
+            self.clearWorkspace()
+        cs = self._asChangeset(self, cs)
+        self.csCheckout = cs
+    cs = property(getCS, setCS)
+
+    def _createWorkingChangeset(self):
+        csParent = self.csCheckout
+        if csParent is None:
+            csParent = self._asChangeset(self, None)
+
+        cs = csParent.newChild()
+        self._csWorking = cs
+        self._updateCurrentChangeset()
+        return cs
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~ Title 
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def checkCommited(self, fail=True):
+        csw = self.getCSWorking(False)
+        if csw is None: return True
+        if not fail: return False
+        raise WorkspaceError("Uncommited changeset")
+
+    def checkout(self, cs=False):
+        self.checkCommited()
+
+        if cs is False:
+            cs = self.csCheckout
+        else: cs = self._asChangeset(cs)
+
+        self.clearWorkspace()
+        self.fetchVersions(cs)
+
+        self.csCheckout = cs
+        self._updateCurrentChangeset()
+        return cs
+
+    def commit(self, **kw):
+        cs = self.getCSWorking(False)
+        if cs is None:
+            return False
+        ns = self.ns
+
+        if kw: cs.update(kw)
+        cs.updateState('closed')
+        self.clearLog()
+        self.csWorking = None
+        self.csCheckout = cs
+        self._updateCurrentChangeset()
+
+        self._dbCommit()
+        return True
+
+    def revertAll(self):
+        cs = self.getCSWorking(False)
+        if cs is not None:
+            cs.revert()
+        self.csWorking = None
+        self.clearLog()
+        self.clearWorkspace()
+        self._updateCurrentChangeset()
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~ template methods to effect workspace ~~~~~~~~~~~~~
+
+    def _updateCurrentChangeset(self):
+        raise NotImplementedError('Subclass Responsibility: %r' % (self,))
+    def fetchVersions(self, cs):
+        raise NotImplementedError('Subclass Responsibility: %r' % (self,))
+    def clearWorkspace(self):
+        raise NotImplementedError('Subclass Responsibility: %r' % (self,))
+    def clearLog(self):
+        raise NotImplementedError('Subclass Responsibility: %r' % (self,))
+    def _dbCommit(self):
+        raise NotImplementedError('Subclass Responsibility: %r' % (self,))
+
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~ OID data operations
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    def read(self, oid, asNS=True):
+        raise NotImplementedError('Subclass Responsibility: %r' % (self,))
+    def readAll(self, asNS=True):
+        raise NotImplementedError('Subclass Responsibility: %r' % (self,))
+    def write(self, oid, **data):
+        raise NotImplementedError('Subclass Responsibility: %r' % (self,))
+    def remove(self, oid):
+        raise NotImplementedError('Subclass Responsibility: %r' % (self,))
+
--- be/sqlite/workspace.py
+++ be/sqlite/workspace.py
@@ -5,8 +5,9 @@
 import sqlite3
 
 from ..base.errors import WorkspaceError
-from .metadata import metadataView
-from .workspaceSchema import WorkspaceSchema
+from ..base.workspace import WorkspaceBase
+from . import metadata
+from . import workspaceSchema
 from .changeset import Changeset
 
 from . import workspaceChangeOps as Ops
@@ -15,27 +16,33 @@ from . import workspaceChangeOps as Ops
 #~ Workspace concept
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-class Workspace(object):
-    Schema = WorkspaceSchema
+class Workspace(WorkspaceBase):
+    Schema = workspaceSchema.WorkspaceSchema
+    metadataView = metadata.metadataView
     temporary = False
 
     def __init__(self, host, wsid='main'):
         self.setHost(host)
         self.wsid = wsid
         self._initSchema()
-        self._initMeta()
+        self._initStoredCS()
 
     def setHost(self, host):
         self.ns = host.ns.copy()
         self.conn = host.conn
         self.cur = host.cur
 
+    def _asChangeset(self, cs=False):
+        if cs is False:
+            return self.cs
+        return Changeset.fromArg(self, cs)
+
     def _initSchema(self):
         schema = self.Schema(self.ns, self)
         schema.initStore(self.conn)
 
-    def _initMeta(self):
-        cs = self.getMetaChangeset()
+    def _initStoredCS(self):
+        cs = self.readCurrentChangeset()
         if cs is None:
             return
 
@@ -47,99 +54,40 @@ class Workspace(object):
             self.csCheckout = cs.csParent
             self.csWorking = cs
 
-    metadataView = metadataView
-
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-    csCheckout = None
-
-    _csWorking = None
-    def getCSWorking(self, create=True):
-        cs = self._csWorking
-        if cs is None and create:
-            csParent = self.csCheckout
-            if csParent is None:
-                csParent = self._changesetFromArg(self, None)
-
-            cs = csParent.newChild()
-            self._csWorking = cs
-            self.updateMetaChangeset()
-        return cs
-    def setCSWorking(self, cs):
-        self._csWorking = cs
-    csWorking = property(getCSWorking, setCSWorking)
-
-    def getCS(self):
-        r = self._csWorking 
-        if r is None:
-            r = self.csCheckout
-        return r
-    def setCS(self, cs):
-        self.checkCommited()
-        if self.csCheckout is not None:
-            self.clearWorkspace()
-        cs = self._changesetFromArg(self, cs)
-        self.csCheckout = cs
-    cs = property(getCS, setCS)
-
-    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    def getMetaChangeset(self):
+    def readCurrentChangeset(self):
         wsMeta = self.metadataView("workspaces")
         cs = wsMeta.getAt(self.wsid, None)
         if cs is not None:
-            return self._changesetFromArg(self, cs)
-    def setMetaChangeset(self, cs, force=False):
+            return self._asChangeset(cs)
+    def saveCurrentChangeset(self, cs=False, force=False):
+        if cs is False:
+            cs = self.cs
         if self.temporary and not force:
             return False
 
         wsMeta = self.metadataView("workspaces")
         if cs is not None:
-            wsMeta[self.wsid] = int(cs)
+            wsMeta[self.wsid] = long(cs)
         else: del wsMeta[self.wsid]
         return True
-    metaChangeset = property(getMetaChangeset, setMetaChangeset)
-
-    def updateMetaChangeset(self):
-        cs = self.cs
-        self.setMetaChangeset(cs)
 
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~ overridden template methods ~~~~~~~~~~~~~~~~~~~~~~
 
-    _changesetFromArg = staticmethod(Changeset.fromArg)
-    def checkout(self, cs=False):
-        self.checkCommited()
+    def _updateCurrentChangeset(self):
+        return self.saveCurrentChangeset()
 
-        if cs is False:
-            cs = self.csCheckout
-        else: cs = self._changesetFromArg(self, cs)
-
-        self.clearWorkspace()
-        versions = [v for v,s in cs.iterLineage() if s != 'mark']
-
-        self.fetchVersions(versions, 'ignore')
-
-        self.csCheckout = cs
-        self.updateMetaChangeset()
-        self._versionsToMark = versions
-        return cs
-
-    def fetchVersions(self, iterVersions, onConflict='ignore'):
-        ns = self.ns.copy()
-        ns.update(onConflict=onConflict)
+    def fetchVersions(self, cs):
+        versions = [(v,) for v,s in cs.iterLineage() if s != 'mark']
 
         res = self.cur.executemany("""\
-            insert or %(onConflict)s into %(ws_version)s
+            replace into %(ws_version)s
                 select * from %(qs_manifest)s
-                    where versionId=?;""" % ns, 
-                    ((v,) for v in iterVersions))
+                    where versionId=?;""" % self.ns, versions)
+        self._versionsToMark = versions
         return res
-
-    def checkCommited(self, fail=True):
-        csw = self.getCSWorking(False)
-        if csw is None: return True
-        if not fail: return False
-        raise WorkspaceError("Uncommited changeset")
 
     def clearWorkspace(self):
         self.checkCommited()
@@ -152,31 +100,14 @@ class Workspace(object):
 
         ex("delete from %(ws_version)s;"%ns)
 
-    def revertAll(self):
-        cs = self.getCSWorking(False)
-        if cs is not None:
-            cs.revert()
-        self.csWorking = None
-        self.cur.execute("delete from %(ws_log)s;")
-        self.clearWorkspace()
-        self.updateMetaChangeset()
+    def clearLog(self):
+        self.cur.execute("delete from %(ws_log)s;" % self.ns)
 
-    def commit(self, **kw):
-        cs = self.getCSWorking(False)
-        if cs is None:
-            return False
-        ns = self.ns
-
-        if kw: cs.update(kw)
-        cs.updateState('closed')
-        self.cur.execute("delete from %(ws_log)s;" % ns)
-        self.csWorking = None
-        self.csCheckout = cs
-        self.updateMetaChangeset()
-
+    def _dbCommit(self):
         self.conn.commit()
-        return True
 
+    #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    #~ OID data operations
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     def read(self, oid, asNS=True):
