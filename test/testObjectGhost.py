1301c81e100f9f9b7a40458657db8a49a3d1a88a
"Shane Holloway ext:(%22) <shane.holloway@ieee.org>
2010-08-28 23:40:21 -0600

    Improved testGhostObject for read error detection

--- test/testObjectGhost.py
+++ test/testObjectGhost.py
@@ -11,7 +11,7 @@
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 import os, sys
-from TG.quicksilver.boundary import BoundaryVersions
+from TG.quicksilver.boundary import BoundaryVersions, OidLookupError
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~ Definitions 
@@ -24,6 +24,16 @@ class GhostItem(object):
         return True
     def answer(self):
         return (6,7)
+
+class NormalItem(object):
+    def _boundary_(self, bndS, bndCtx):
+        return True
+
+    def isOptionalItem(self):
+        return False
+
+    def answer(self):
+        return 1942
 
 class OptionalItem(object):
     def _boundary_(self, bndS, bndCtx):
@@ -44,8 +54,6 @@ class OptionalItem(object):
     def isOptionalItem(self):
         return True
 
-_OptionalItem = OptionalItem
-
 def openBoundaryStore():
     dbFn = os.path.splitext(__file__)[0]+'.qag'
     qsh = BoundaryVersions(dbFn, 'obj')
@@ -56,7 +64,9 @@ def openBoundaryStore():
 def testStore():
     bs = openBoundaryStore()
     root = OptionalItem()
+    root.state = (2,3,7)
     bs.set(100, root)
+    bs.set(101, NormalItem())
     bs.commit()
 
     assert root.isOptionalItem(), root
@@ -67,23 +77,87 @@ def testReadback():
     root = bs.get(100)
     assert root.isOptionalItem(), root
     assert root.answer() == 42
+    assert root.state == (2,3,7)
+
+    r2 = bs.get(101)
+    assert not r2.isOptionalItem(), r2
+    assert r2.answer() == 1942
+
+def testNoGhost():
+    global NormalItem
+    _NormalItem = NormalItem
+    del NormalItem
+
+    try:
+        bs = openBoundaryStore()
+        root = bs.get(100)
+        assert root.isOptionalItem(), root
+        assert root.answer() == 42
+        assert root.state == (2,3,7)
+
+        try:
+            r2 = bs.get(101)
+            assert False, "Should have raised an error"
+        except OidLookupError, err:
+            print err
+    finally:
+        NormalItem = _NormalItem
+
 
 def testGhost():
     global OptionalItem
+    _OptionalItem = OptionalItem
     del OptionalItem
 
-    bs = openBoundaryStore()
-    root = bs.get(100)
-    assert not root.isOptionalItem(), root
-    assert root.isGhost(), root
-    assert root.answer() == (6,7)
+    try:
+        bs = openBoundaryStore()
+        root = bs.get(100)
+        assert not root.isOptionalItem(), root
+        assert root.isGhost(), root
+        assert root.answer() == (6,7)
+    finally:
+        OptionalItem = _OptionalItem
+
+def testGhostFail():
+    global GhostItem
+    _GhostItem = GhostItem
+    del GhostItem
+
+    try:
+        bs = openBoundaryStore()
+        try:
+            root = bs.get(100)
+            assert False, "Should have raised an error"
+        except OidLookupError, err:
+            print err
+
+        r2 = bs.get(101)
+        assert not r2.isOptionalItem(), r2
+        assert r2.answer() == 1942
+
+    finally:
+        GhostItem = _GhostItem
 
 def testGhostRestore():
-    global OptionalItem
-    OptionalItem = _OptionalItem
-
     bs = openBoundaryStore()
     root = bs.get(100)
     assert root.isOptionalItem(), root
     assert root.answer() == 42
+    assert root.state == (2,3,7)
+
+    r2 = bs.get(101)
+    assert not r2.isOptionalItem(), r2
+    assert r2.answer() == 1942
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+def excepthook(excType, exc, tb): 
+    pass
+
+def setup():
+    excepthook.state = (sys.excepthook, sys.stderr)
+    sys.stderr = sys.stdout
+def teardown():
+    sys.excepthook, sys.stderr = excepthook.state
+    excepthook.state = None
 
